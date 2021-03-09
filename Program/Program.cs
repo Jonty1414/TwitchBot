9a472e49bcd0f2a6b8b724fc8d2dcb942ba1d4a9
Jonathan Reardon <jonty1414@gmail.com>
2021-03-09 14:54:33 +1300

    Add project files.

---
+++ Program/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CSGSI;
+using CSGSI.Nodes;
+using TwitchLib;
+using TwitchLib.Client;
+
+namespace Program
+{
+    class Program
+    {
+        static GameStateListener gsl;
+        static TwitchBot mybot;
+        static void Main(string[] args)
+        {
+            mybot = new TwitchBot();
+            gsl = new GameStateListener(3000);
+            gsl.NewGameState += new NewGameStateHandler(OnNewGameState);
+            if (!gsl.Start())
+            {
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Listening...");
+        }
+
+        public static void OnNewGameState(GameState gs)
+        {
+
+            Console.WriteLine("New Game State");
+            if (gs.Round.Phase == RoundPhase.Live && 
+                gs.Player.State.RoundKills == 3 && 
+                gs.Previously.Player.State.RoundKills == 2)
+            {
+                mybot.MessageChat("woooh, you just got a 3k!!!!");
+                Console.WriteLine("3k");
+            }
+        }
+    }
+}
---
+++ Program/TwitchBot.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TwitchLib.Client;
+using TwitchLib.Client.Enums;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Extensions;
+using TwitchLib.Client.Models;
+using TwitchLib.Api;
+using TwitchLib.Api.Core;
+using TwitchLib.Api.Helix;
+using TwitchLib.Api.Services;
+using TwitchLib.Api.Services.Events;
+using TwitchLib.Api.Services.Events.LiveStreamMonitor;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace Program
+{
+    class TwitchBot
+    {
+        TwitchClient client;
+        TwitchAPI API;
+
+        public TwitchBot()
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials(TwitchInfo.BotUserName, TwitchInfo.AccessToken);
+            var clientOptions = new ClientOptions
+            {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(30)
+            };
+            WebSocketClient customClient = new WebSocketClient(clientOptions);
+
+            API = new TwitchAPI();
+            API.Settings.ClientId = TwitchInfo.ClientID;
+            API.Settings.AccessToken = TwitchInfo.AccessToken;
+
+            client = new TwitchClient(customClient);
+            client.Initialize(credentials, TwitchInfo.ChannelName);
+
+            client.OnLog += Client_OnLog;
+            client.OnJoinedChannel += Client_OnJoinedChannel;
+            client.OnMessageReceived += Client_OnMessageReceived;
+            client.OnWhisperReceived += Client_OnWhisperReceived;
+            client.OnNewSubscriber += Client_OnNewSubscriber;
+            client.OnConnected += Client_OnConnected;
+
+            client.Connect();
+        }
+
+        public void MessageChat(string msg)
+        {
+            client.SendMessage(TwitchInfo.ChannelName, msg);
+        }
+
+        private void Client_OnLog(object sender, OnLogArgs e)
+        {
+            Console.WriteLine($"{e.DateTime.ToString()}: {e.BotUsername} - {e.Data}");
+        }
+
+        private void Client_OnConnected(object sender, OnConnectedArgs e)
+        {
+            Console.WriteLine($"Connected to {e.AutoJoinChannel}");
+        }
+
+        private void Client_OnJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+            Console.WriteLine("Hey guys! I am a bot connected via TwitchLib!");
+            client.SendMessage(e.Channel, "Hey guys! I am a bot.");
+        }
+
+        private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            if (e.ChatMessage.Message.Contains("!clip"))
+            {
+                API.Helix.Clips.CreateClipAsync(TwitchInfo.ChannelName, TwitchInfo.AccessToken);
+                client.SendMessage(TwitchInfo.ChannelName, "clip genertated");
+            }
+        }
+
+        private void Client_OnWhisperReceived(object sender, OnWhisperReceivedArgs e)
+        {
+            if (e.WhisperMessage.Username == "my_friend")
+                client.SendWhisper(e.WhisperMessage.Username, "Hey! Whispers are so cool!!");
+        }
+
+        private void Client_OnNewSubscriber(object sender, OnNewSubscriberArgs e)
+        {
+            if (e.Subscriber.SubscriptionPlan == SubscriptionPlan.Prime)
+                client.SendMessage(e.Channel, $"Welcome {e.Subscriber.DisplayName} to the substers! You just earned 500 points! So kind of you to use your Twitch Prime on this channel!");
+            else
+                client.SendMessage(e.Channel, $"Welcome {e.Subscriber.DisplayName} to the substers! You just earned 500 points!");
+        }
+    }
+}
